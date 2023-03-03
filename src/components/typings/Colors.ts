43139f009d7764e8c1feed753118a98f49043404
duart38 <duartsnel@icloud.com>
2023-03-03 12:39:20 +0100

    add enum for safe color selection
    Signed-off-by: duart38 <duartsnel@icloud.com>

---
+++ src/components/typings/Colors.ts
@@ -0,0 +1,37 @@
+/*
+ *   Copyright (c) 2023 Duart Snel
+ *   All rights reserved.
+
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+export type BgColors =
+    | "bg-dark"
+    | "bg-dark-1"
+    | "bg-blue"
+    | "bg-yellow"
+    | "bg-grey"
+    | "bg-green"
+    | "bg-red"
+    | "bg-deep-red"
+    | "bg-purple";
+
+export type TxtColors =
+    | "txt-black"
+    | "txt-dark-1"
+    | "txt-blue"
+    | "txt-yellow"
+    | "txt-grey"
+    | "txt-green"
+    | "txt-white"
+    | "txt-red";
