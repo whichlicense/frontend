84bdf69f188266b822d48a6845acaaee7acac3ed
duart38 <duartsnel@icloud.com>
2023-03-04 10:37:39 +0100

    Started implementing Search page
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -15,10 +15,45 @@
  *   limitations under the License.
  */
 
+import { InputGroup, Form, Row, Col, Stack } from "react-bootstrap";
+import RegularCard from "../components/Cards/RegularCard";
+
 export default function Search() {
-    return (
-        <div>
-            <h1>Search</h1>
-        </div>
-    )
+  return (
+    <div>
+      <RegularCard>
+        {/* TODO: maybe put this in the toolbar? */}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Search..." />
+          <Form.Text className="text-muted">
+            Enter some information about the dependency you want to search for.
+            Use the toolbar above to fine tune your search parameters.
+          </Form.Text>
+        </Form.Group>
+      </RegularCard>
+
+      <br />
+
+      <Row xs={1} md={3} lg={4} xl={5} xxl={7} className="g-3">
+        {Array.from({ length: 40 }).map((_, idx) => (
+          <Col>
+            <RegularCard
+              title={`Example ${idx}`}
+              icon="bi bi-arrow-up-right-circle"
+              iconColor="txt-purple"
+              onCardClick={() => {
+                console.log("clicked");
+              }}
+            >
+              <Stack gap={2}>
+                <h6>Manager: NPM</h6>
+                <small>Last scan: Jan 20, 2023</small>
+                <small>Scanned version: 2.1.0</small>
+              </Stack>
+            </RegularCard>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
