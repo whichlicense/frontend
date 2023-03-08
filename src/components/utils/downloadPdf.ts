18c4905124d4fab2345967631ba18feebe384ea7
duart38 <duartsnel@icloud.com>
2023-03-08 12:49:32 +0100

    removed component specific pdf exports to method
    Signed-off-by: duart38 <duartsnel@icloud.com>

---
+++ src/components/utils/downloadPdf.ts
@@ -0,0 +1,31 @@
+/*
+ *   Copyright (c) 2023 Duart Snel
+ *   All rights reserved.
+
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+// @ts-ignore
+import * as html2pdf from "html2pdf.js";
+
+export function downloadPdf(elementRef: React.MutableRefObject<any>, opts: {fileName: string}){
+    const element = elementRef.current;
+    const options = {
+      margin: 0,
+      filename: opts.fileName,
+      image: { type: "jpeg", quality: 1 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+      pagebreak: { mode: ["avoid-all", "css", "legacy"] },
+    };
+    html2pdf().set(options).from(element).save();
+}
