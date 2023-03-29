46ddee56fff13bd463778b648f3b50eb3defd00a
duart38 <duartsnel@icloud.com>
2023-03-29 13:20:52 +0200

    allow data and custom stroke
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/components/Charts/GoogleTreeMapChart.tsx
+++ src/components/Charts/GoogleTreeMapChart.tsx
@@ -18,7 +18,16 @@ import { Chart } from "react-google-charts";
 import "../../styles/Chart.css"
 
 type TGoogleTreeMapChartProps = {
-    
+  /**
+   * The data to be displayed in the chart.
+   * ```
+   * [
+   *     [ 'ID', 'Parent ID', size ],
+   *    // ...
+   * ]
+   */
+    data: [string, string | null, number][],
+    bg: "bg-section" | "bg-card",
 };
 export function GoogleTreeMapChart(props: TGoogleTreeMapChartProps) {
   const data = [
@@ -27,34 +36,7 @@ export function GoogleTreeMapChart(props: TGoogleTreeMapChartProps) {
       "Parent",
       "Size",
     ],
-    ["Global", null, 0],
-    ["America", "Global", 0],
-    ["Europe", "Global", 0],
-    ["Asia", "Global", 0],
-    ["Australia", "Global", 0],
-    ["Africa", "Global", 0],
-    ["Brazil", "America", 11],
-    ["USA", "America", 52],
-    ["Mexico", "America", 24],
-    ["Canada", "America", 16,],
-    ["France", "Europe", 42,],
-    ["Germany", "Europe", 31,],
-    ["Sweden", "Europe", 22,],
-    ["Italy", "Europe", 17],
-    ["UK", "Europe", 21,],
-    ["China", "Asia", 36],
-    ["Japan", "Asia", 20,],
-    ["India", "Asia", 400],
-    ["Laos", "Asia", 4],
-    ["Mongolia", "Asia", 1,],
-    ["Israel", "Asia", 12],
-    ["Iran", "Asia", 18],
-    ["Pakistan", "Asia", 11,],
-    ["Egypt", "Africa", 21],
-    ["S. Africa", "Africa", 30],
-    ["Sudan", "Africa", 12],
-    ["Congo", "Africa", 10],
-    ["Zaire", "Africa", 8],
+    ...props.data
   ];
 
   const options: Chart["props"]["options"] = {
@@ -74,10 +56,10 @@ export function GoogleTreeMapChart(props: TGoogleTreeMapChartProps) {
     <Chart
       chartType="TreeMap"
       width="100%"
-      height="400px"
+      height="40vh"
       data={data}
       options={options}
-      className="wl-google-chart"
+      className={`wl-google-chart ${props.bg}`}
     />
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -66,7 +66,40 @@ export default function Dashboard() {
         <div className="ps-2">
           <SectionHeading title="Package exposure" size="6" type="display" />
         </div>
-        <GoogleTreeMapChart />
+        <GoogleTreeMapChart
+          bg="bg-section"
+         data={
+          [
+            ["Global", null, 0],
+            ["America", "Global", 0],
+            ["Europe", "Global", 0],
+            ["Asia", "Global", 0],
+            ["Australia", "Global", 0],
+            ["Africa", "Global", 0],
+            ["Brazil", "America", 11],
+            ["USA", "America", 52],
+            ["Mexico", "America", 24],
+            ["Canada", "America", 16,],
+            ["France", "Europe", 42,],
+            ["Germany", "Europe", 31,],
+            ["Sweden", "Europe", 22,],
+            ["Italy", "Europe", 17],
+            ["UK", "Europe", 21,],
+            ["China", "Asia", 36],
+            ["Japan", "Asia", 20,],
+            ["India", "Asia", 400],
+            ["Laos", "Asia", 4],
+            ["Mongolia", "Asia", 1,],
+            ["Israel", "Asia", 12],
+            ["Iran", "Asia", 18],
+            ["Pakistan", "Asia", 11,],
+            ["Egypt", "Africa", 21],
+            ["S. Africa", "Africa", 30],
+            ["Sudan", "Africa", 12],
+            ["Congo", "Africa", 10],
+            ["Zaire", "Africa", 8],
+          ]
+        } />
         </Stack>
         
       </Col>
