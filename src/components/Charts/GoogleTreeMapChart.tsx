9d790121ca9e529a3edbbee16578291f66d92482
duart38 <duartsnel@icloud.com>
2023-04-05 12:20:59 +0200

    all this because of safari. thanks apple
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/components/Charts/GoogleTreeMapChart.tsx
+++ src/components/Charts/GoogleTreeMapChart.tsx
@@ -15,7 +15,7 @@
  *   limitations under the License.
  */
 import { Chart, ReactGoogleChartEvent } from "react-google-charts";
-import "../../styles/Chart.css"
+import "../../styles/Chart.css";
 import { useElementSize } from "../Hooks/useElementSize";
 import { useEffectOnce } from "../utils/useEffectOnce";
 
@@ -28,24 +28,19 @@ type TGoogleTreeMapChartProps = {
    *    // ...
    * ]
    */
-    data: [string, string | null, number][],
-    bg: "bg-section" | "bg-card",
-    resizableContainerId?: string,
-    maxDepth?: number,
-    maxPostDepth?: number,
+  data: [string, string | null, number][];
+  bg: "bg-section" | "bg-card";
+  resizableContainerId?: string;
+  maxDepth?: number;
+  maxPostDepth?: number;
 };
 export function GoogleTreeMapChart(props: TGoogleTreeMapChartProps) {
-  const data = [
-    [
-      "Item",
-      "Parent",
-      "Size",
-    ],
-    ...props.data
-  ];
+  const data = [["Item", "Parent", "Size"], ...props.data];
   // Forces a re-render (and thus resize) when the supplied element is resized.
   useElementSize({ id: props.resizableContainerId || "root", debounce: 5 });
-  const uid = (Math.random().toString(36) + Date.now().toString(36)).substring(2)
+  const uid = (Math.random().toString(36) + Date.now().toString(36)).substring(
+    2
+  );
 
   const options: Chart["props"]["options"] = {
     noColor: "#818b8e",
@@ -65,36 +60,40 @@ export function GoogleTreeMapChart(props: TGoogleTreeMapChartProps) {
   };
 
   return (
-    <div className="position-relative w-100" style={{
-      height: "40vh",
-    }}>
+    <div
+      className="position-relative w-100"
+      style={{
+        height: "40vh",
+      }}
+    >
       <div className="position-absolute w-100" id={uid}>
         <Chart
-        chartType="TreeMap"
-        width={`100%`}
-        height="40vh"
-        data={data}
-        options={options}
-        className={`wl-google-chart ${navigator.userAgent.includes("Safari") ? "" : "not-safari"} ${props.bg}`}
-        onLoad={() => {
-          // I can't believe I have to do this...
-          if(navigator.userAgent.includes("Safari")){
-            // I hate safari.
-            setTimeout(() => {
-              const x = document.getElementById(uid);
-              if(!x) return;
-              const res = x!.querySelectorAll("rect")
-              
-              for(const element of res.values()){
-                element.setAttribute("rx", "12px")
-                element.setAttribute("ry", "12px")
-              }
-            }, 1000)
-          }
-        }}
-      />
+          chartType="TreeMap"
+          width={`100%`}
+          height="40vh"
+          data={data}
+          options={options}
+          className={`wl-google-chart ${
+            navigator.userAgent.includes("Safari") ? "" : "not-safari"
+          } ${props.bg}`}
+          onLoad={() => {
+            // I can't believe I have to do this...
+            if (navigator.userAgent.includes("Safari")) {
+              // I hate safari.
+              setTimeout(() => {
+                const x = document.getElementById(uid);
+                if (!x) return;
+                const res = x!.querySelectorAll("rect");
+
+                for (const element of res.values()) {
+                  element.setAttribute("rx", "12px");
+                  element.setAttribute("ry", "12px");
+                }
+              }, 400);
+            }
+          }}
+        />
       </div>
-    
     </div>
   );
 }
