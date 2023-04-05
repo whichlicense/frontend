150f705377e600e87755c5b809ef4776ff8e451c
duart38 <duartsnel@icloud.com>
2023-04-05 12:16:01 +0200

    [patch] nasty fix for shitty safari
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/components/Charts/GoogleTreeMapChart.tsx
+++ src/components/Charts/GoogleTreeMapChart.tsx
@@ -14,9 +14,10 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-import { Chart } from "react-google-charts";
+import { Chart, ReactGoogleChartEvent } from "react-google-charts";
 import "../../styles/Chart.css"
 import { useElementSize } from "../Hooks/useElementSize";
+import { useEffectOnce } from "../utils/useEffectOnce";
 
 type TGoogleTreeMapChartProps = {
   /**
@@ -44,6 +45,7 @@ export function GoogleTreeMapChart(props: TGoogleTreeMapChartProps) {
   ];
   // Forces a re-render (and thus resize) when the supplied element is resized.
   useElementSize({ id: props.resizableContainerId || "root", debounce: 5 });
+  const uid = (Math.random().toString(36) + Date.now().toString(36)).substring(2)
 
   const options: Chart["props"]["options"] = {
     noColor: "#818b8e",
@@ -61,18 +63,35 @@ export function GoogleTreeMapChart(props: TGoogleTreeMapChartProps) {
     maxDepth: props.maxDepth || 1,
     maxPostDepth: props.maxPostDepth || 0,
   };
+
   return (
     <div className="position-relative w-100" style={{
       height: "40vh",
     }}>
-      <div className="position-absolute w-100">
+      <div className="position-absolute w-100" id={uid}>
         <Chart
         chartType="TreeMap"
         width={`100%`}
         height="40vh"
         data={data}
         options={options}
-        className={`wl-google-chart ${props.bg}`}
+        className={`wl-google-chart ${navigator.userAgent.includes("Safari") ? "" : "not-safari"} ${props.bg}`}
+        onLoad={() => {
+          // I can't believe I have to do this...
+          if(navigator.userAgent.includes("Safari")){
+            // I hate safari.
+            setTimeout(() => {
+              const x = document.getElementById(uid);
+              if(!x) return;
+              const res = x!.querySelectorAll("rect")
+              
+              for(const element of res.values()){
+                element.setAttribute("rx", "12px")
+                element.setAttribute("ry", "12px")
+              }
+            }, 1000)
+          }
+        }}
       />
       </div>
     
