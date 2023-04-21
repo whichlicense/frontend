cc744c6e9d8a79b2ccc66561e68e5fce43061898
duart38 <duartsnel@icloud.com>
2023-04-21 12:02:06 +0200

    changed port
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- public/electron.js
+++ public/electron.js
@@ -37,7 +37,7 @@ function createWindow() {
   // win.loadFile("index.html");
   win.loadURL(
     isDev
-      ? 'http://localhost:3006'
+      ? 'http://localhost:3007'
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
   // Open the DevTools.
